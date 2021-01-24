e201c27f87f477f6bcb84e287dc1998980d99f54
ghost-lolade <horlorlerdey10@gmail.com>
2021-01-24 10:07:01 +0100

    Created menu items and dynamic button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+
+  const allCategories = ['all', ...new Set(items.map((item)=>item.category))]
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories)
+
+  // console.log(allCategories)
+  const filterItems = (category) => {
+    if (category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems)
+  };
+  return (
+    <main>
+      <section className='menu section'>
+        <div  className='title'>
+          <h2>ghost menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories}/>
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
